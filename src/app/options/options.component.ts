cbae0e4258224d7b0b21964fc718f57beb98cfe3
alexNeto <alex.neto3@hotmail.com>
2017-07-29 19:45:03 +0000

    add animation and configutation div

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -10,13 +10,39 @@ import { Component} from '@angular/core';
 export class OptionsComponent {
 
   passStrength = '';
+  imgPath = '././assets/img/down.png';
+  configButton = 'configButtonOFF';
+  config = 'configOFF';
+  roundshadow = '';
 
+  state = false;
+  showConfig(){
+    if(this.state){
+      this.state = false;
+      this.imgPath = '././assets/img/down.png';
+      this.configButton = 'configButtonOFF';
+      this.config = 'configOFF';
+      this.roundshadow = '';
+    }
+    else{
+      this.state = true;
+      this.imgPath = '././assets/img/up.png';
+      this.configButton = 'configButtonON';
+      this.config = 'config';
+      this.roundshadow = 'roundshadow';
 
+    }
+  }
   myEvent(){
-    var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < 9; i++)
+    var text = "";
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lower = "abcdefghijklmnopqrstuvwxyz";
+    var number = "0123456789";
+    var special = "!@#$%*()[{/}]"
+    var possible = upper + lower + number + special;
+
+    for (var i = 0; i < 9; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     this.passStrength = text;
