9fdf09c2597913d38816721f57d82d7907f0ebdd
alexNeto <alex.neto3@hotmail.com>
2017-07-29 15:09:51 +0000

    something works

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-options',
   templateUrl: './options.component.html',
   styleUrls: ['./options.component.css']
 })
-export class OptionsComponent implements OnInit {
 
-  constructor() { }
+export class OptionsComponent {
 
-  ngOnInit() {
+  passStrength = '';
+
+
+  myEvent(){
+    var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 9; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    this.passStrength = text;
   }
 
 }
