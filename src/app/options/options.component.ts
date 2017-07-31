4d451245e55075b2d2f097120b87d7fc8efb72f6
alexNeto <alex.neto3@hotmail.com>
2017-07-31 15:36:09 +0000

    changing config

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -8,23 +8,21 @@ import { Component} from '@angular/core';
 
 export class OptionsComponent {
 
-  min_input:number;
-  max_input:number;
-  min_length:number;
-  max_length:number;
-
   value:number = 50;
+  passSizeValue:number = 9;
 
-transperancy(Transvalue){
-    this.value = Transvalue;
+  passSize(passSizeValue){
+    this.passSizeValue = passSizeValue;
+  }
+
+strength(StrengthValue){
+    this.value = StrengthValue;
 }
 
   passStrength = '';
   imgPath = '././assets/img/down.png';
   configButton = 'configButtonOFF';
   config = 'configOFF';
-  roundshadow = '';
-  showStrength = 'color1';
 
   state = false;
   showConfig(){
@@ -33,14 +31,12 @@ transperancy(Transvalue){
       this.imgPath = '././assets/img/down.png';
       this.configButton = 'configButtonOFF';
       this.config = 'configOFF';
-      this.roundshadow = '';
     }
     else{
       this.state = true;
       this.imgPath = '././assets/img/up.png';
       this.configButton = 'configButtonON';
       this.config = 'config';
-      this.roundshadow = 'roundshadow';
 
     }
   }
@@ -50,48 +46,36 @@ transperancy(Transvalue){
     var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var lower = "abcdefghijklmnopqrstuvwxyz";
     var number = "0123456789";
-    var special = "!@#$%*()[{/}]"
+    var special = "()`~!@#$%^&*-+=|\\{}[]:;\"'<>,.?/";
     var possible = '';
-    var passlength = 1;
 
     if(this.value > 0){
       possible += lower;
-      passlength = 9;
-      passlength = 9;
 
     }
     if(this.value > 10){
       possible += number;
-      passlength = 9;
     }
     if(this.value > 20){
       possible += upper;
-      passlength = 9;
     }
     if(this.value > 30){
       possible += special;
-      passlength = 9;
     }
     if(this.value > 40){
-      passlength = 12;
     }
     if(this.value > 50){
-      passlength = 16;
     }
     if(this.value > 60){
-      passlength = 21;
     }
     if(this.value > 70){
-      passlength = 27;
     }
     if(this.value > 80){
-      passlength = 33;
     }
     if(this.value > 90){
-      passlength = 48;
     }
 
-    for (var i = 0; i < passlength; i++)
+    for (var i = 0; i < this.passSizeValue; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     this.passStrength = text;
